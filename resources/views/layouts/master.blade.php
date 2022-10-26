f3ead77de06b0dd1e6b2660e6fb98726de86663e
Jucimara Pereira <maricimara98@outlook.com>
2022-10-26 11:38:56 -0300

    :sparkles: feat: Layout das páginas

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!-- Stored in resources/views/layouts/master.blade.php -->
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="maricimara98">
+    <title>GuessNumber</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <script src="./js/index.js" defer></script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <main class="container mt-5">
+        @yield('content')
+    </main>
+
+</body>
+
+</html>
